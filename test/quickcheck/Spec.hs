dec2c5dbe68a2e7b33b80f0daf03a03a3aa3b419
MATSUBARA Nobutada <nobutada0507@gmail.com>
2024-04-22 14:58:29 +0900

    sample quickcheck

--- test/quickcheck/Spec.hs
+++ test/quickcheck/Spec.hs
@@ -1,2 +1,30 @@
+import Lib
+
+import Numeric (showFFloat)
+import Test.QuickCheck
+import Text.Megaparsec (parseMaybe)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  quickCheck propParseNationalNumber
+  quickCheck propParseRationalNumber
+
+propParseNationalNumber :: Integer -> Bool
+propParseNationalNumber n
+  | n >= 0    = parseMaybe natParser (show n) == Just n
+  | otherwise = parseMaybe natParser (show $ (-1) * n) == Just ((-1) * n)
+
+-- | Failuer case
+-- propParseNationalNumber :: Integer -> Bool
+-- propParseNationalNumber n = parseMaybe natParser (show n) == Just n
+
+propParseRationalNumber :: Double -> Bool
+propParseRationalNumber n =
+  fmap fromRational (parseMaybe rationalParser (showFFloat Nothing n "")) == Just n
+
+-- | Failuer case
+-- propParseRationalNumber :: Rational -> Bool
+-- propParseRationalNumber n =
+--   parseMaybe rationalParser (show n') == Just n
+--   where
+--     n' = fromRational n :: Double
